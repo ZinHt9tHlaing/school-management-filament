90d71414538d0e7d38598623dcb106a412f8c375
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-09-10 15:18:58 +0630

    user crud added

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -10,7 +12,17 @@ class UserForm
     {
         return $schema
             ->components([
-                //
+                TextInput::make('name')->required()->maxLength(255),
+                TextInput::make('email')->required()->email(),
+                Select::make('role_id')->label('Role')
+                    ->relationship('role', 'name') // assumes Role model has a 'name' field
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->revealable() // show reveal button
+                    ->required(fn($context) => $context === 'create') // only required on create
+                    ->minLength(6)
+                    ->dehydrated(fn($state) => filled($state)), // store only if filled
             ]);
     }
 }
